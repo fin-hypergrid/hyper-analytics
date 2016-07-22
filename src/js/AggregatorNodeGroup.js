822e38c8b6c3f637fe317a47109571a0d0b219ac
Dwayne Jones <dwayne@openfin.co>
2016-07-22 00:54:33 -0400

    Refixed aggregation

--- src/js/AggregatorNodeGroup.js
+++ src/js/AggregatorNodeGroup.js
@@ -7,6 +7,15 @@ var DataNodeGroup = require('./DataNodeGroup');
  * @constructor
  * @extends DataNodeBase
  */
-var AggregatorNodeGroup = DataNodeGroup.extend('AggregatorNodeGroup', AggregatorNodeBaseMixin);
+var AggregatorNodeGroup = DataNodeGroup.extend('AggregatorNodeGroup', {
+    getRowData: function (drillDown) {
+        AggregatorNodeBaseMixin.getRowData.apply(this, arguments);
+        if (this.expanded) {
+            this.children.forEach(function (child) {
+                child.getRowData(drillDown);
+            });
+        }
+    }
+});
 
 module.exports = AggregatorNodeGroup;
--- src/js/AggregatorNodeTree.js
+++ src/js/AggregatorNodeTree.js
@@ -7,6 +7,15 @@ var DataNodeTree = require('./DataNodeTree');
  * @constructor
  * @extends DataNodeBase
  */
-var AggregatorNodeTree = DataNodeTree.extend('AggregatorNodeTree', AggregatorNodeBaseMixin);
+var AggregatorNodeTree = DataNodeTree.extend('AggregatorNodeTree', {
+    getRowData: function (drillDown) {
+        AggregatorNodeBaseMixin.getRowData.apply(this, arguments);
+        if (this.expanded) {
+            this.children.forEach(function (child) {
+                child.getRowData(drillDown);
+            });
+        }
+    }
+});
 
 module.exports = AggregatorNodeTree;

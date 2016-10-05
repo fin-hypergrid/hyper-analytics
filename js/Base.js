1720199357f7b3b3cc7273164ee89455b0e7b994
Jonathan Eiten <jonathan@openfin.co>
2016-10-05 12:06:57 -0400

    bumped base version

--- js/Base.js
+++ js/Base.js
@@ -2,34 +2,35 @@
 
 var Base = require('fin-hypergrid-data-source-base');
 
-// Following is temporary until new DataBaseOrigin
+// Following are for legacy methods
+
 Base.prototype.getFields = function() {
     if (this.dataSource) {
-        return this.dataSource.getFields.apply(this.dataSource, arguments);
+        return this.dataSource.getFields();
     }
 };
 
 Base.prototype.getHeaders = function() {
     if (this.dataSource) {
-        return this.dataSource.getHeaders.apply(this.dataSource, arguments);
+        return this.dataSource.getHeaders();
     }
 };
 
 Base.prototype.getCalculators = function() {
     if (this.dataSource) {
-        return this.dataSource.revealRow.apply(this.dataSource, arguments);
+        return this.dataSource.revealRow();
     }
 };
 
 Base.prototype.setFields = function(arr) {
     if (this.dataSource) {
-        return this.dataSource.setFields.apply(this.dataSource, arguments);
+        return this.dataSource.setFields.call(this.dataSource, arr);
     }
 };
 
 Base.prototype.setHeaders = function(arr) {
     if (this.dataSource) {
-        return this.dataSource.setHeaders.apply(this.dataSource, arguments);
+        return this.dataSource.setHeaders.call(this.dataSource, arr);
     }
 };
 
